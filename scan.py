889055856f71414797877553e68f326d5e86b779
sven wang <erfzsven@126.com>
2014-12-15 19:54:11 +0800

    Update scan.py
    complete this function

--- scan.py
+++ scan.py
@@ -1,16 +1,40 @@
 #confirmed 7 digitals are formed as 28 bit , 0xFFFFFFF in hex, not just any ACSII number or letter.
 #so i will pick lower 16 bit, that is 65535 values, enough for our usage.
 #bar scaner is just like a keyboard, put what is scaned and adding a \n.
-#wistron sn standard is like: "555M009D01G450000028J0SA"
+#wistron sn standard is like: "555M009D01G450000028J0SA", i pick "0028"
+#usage: python programming.py com_port_number
+#plug arduino before executing programming.py
 
-import time
 import serial
+import time
 
-t = time.localtime()
-print(t.tm_mon, t.tm_day, t.tm_hour, t.tm_min, t.tm_sec, end = ': ')
-sn = input("scan SN: ")[ 15 : 19]
-bytes.fromhex(sn)
-
-
-if "__name__" == "__main__":
-        __main__()
+def programming(comx):
+	""" plug in arduino before executing programming,py
+usage: under scripts's folder, open cmd, type: python scan.py comx
+	"""
+	ser = serial.Serial(comx - 1)
+	while True:
+		t = time.localtime()
+		print(t.tm_year, '/', t.tm_mon, '/', t.tm_mday, ' ',
+		"{:>2}".format(t.tm_hour), ':', "{:>2}".format(t.tm_min),
+		':', "{:>2}".format(t.tm_sec), end = ' ', sep = '')
+		
+		sn = input(" scan SN: ")
+		sn_send = bytes.fromhex(sn[16:20])
+		if ser.write(sn_send) != 2:
+			print("SN is incorrect! Check!")
+			ser.close()
+			break
+		while ser.inWaiting() == 0:
+			pass
+		else:
+			if ser.read() != b'\x50':
+				print("----------------------------------------------------NG")
+				break
+			else:
+				print("----------------------------------------------------PASS")
+				
+				
+if __name__ == "__main__":
+	import sys
+	programming( int( sys.argv[1] ) )
