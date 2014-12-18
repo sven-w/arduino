28d85b2572c8a8eebaaa6abcc41df87c766f73ac
sven wang <erfzsven@126.com>
2014-12-18 09:34:46 +0800

    Update arduino_eeprom.ino
    fix style

--- arduino_eeprom.ino
+++ arduino_eeprom.ino
@@ -10,13 +10,13 @@
 byte serialno[12] = {0x4a, 0, 0, 1, 0x3a, 0, 0, 1, 0x3b, 0, 0, 1};
 void setup()
 {
-    Serial.begin(9600);
-    digitalWrite(13, LOW);
-    for( unsigned int a = 0; a < 12; a++){
+  Serial.begin(9600);
+  digitalWrite(13, LOW);
+  for( unsigned int a = 0; a < 12; a++){
     EEPROM.write(a, serialno[a]);
     Serial.print(serialno[a], HEX);
     delay(10);
-    }
+  }
   Serial.print('\n');
   int v = 0;
   for(unsigned int b = 0; b < 16; b++){
