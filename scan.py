ac2a28559a1a0a5180ce486e862ba2165ccf9874
sven wang <erfzsven@126.com>
2015-01-05 16:19:28 +0800

    Update scan.py
    fixed serial number digital positons.

--- scan.py
+++ scan.py
@@ -1,7 +1,7 @@
 #confirmed 7 digitals are formed as 28 bit , 0xFFFFFFF in hex, not just any ACSII number or letter.
 #so i will pick lower 16 bit, that is 65535 values, enough for our usage.
 #bar scaner is just like a keyboard, put what is scaned and adding a \n.
-#wistron sn standard is like: "555M009D01G450000028J0SA", i pick "0028"
+#wistron sn standard is like: "555M009D01G45000028J0SA", i pick "0028"
 #usage: python programming.py com_port_number
 #plug arduino before executing programming.py
 #test with VSPE and putty for serial communication
@@ -21,7 +21,7 @@ usage: under scripts's folder, open cmd, type: python scan.py comx
 		':', str(t.tm_sec).zfill(2), end = ' ', sep = '')
 		
 		sn = input(" scan SN: ")
-		sn_send = bytes.fromhex(sn[16:20])
+		sn_send = bytes.fromhex(sn[15:19])
 		if ser.write(sn_send) != 2:
 			print("SN is incorrect! Check!")
 			ser.close()
